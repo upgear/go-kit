66627e85f2dff311eefe4c0d1daa87922b633f4c
Nick Stogner <nicholas.stogner@gmail.com>
2017-05-22 22:28:31 -0400

    Add web.Redo example

---
+++ web/example_test.go
@@ -0,0 +1,22 @@
+package web_test
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/upgear/go-kit/web"
+)
+
+func Example_Redo() {
+	req, err := http.NewRequest("GET", "https://golang.org", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Try to 3 times to get a 2XX back
+	resp, err := web.Redo(3, http.DefaultClient, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+}
